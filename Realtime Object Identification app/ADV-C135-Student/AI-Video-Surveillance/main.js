ddabe374c6cbcf364bb16445b895f4517fbe1190
GuhLazy <89132745+GuhLazy@users.noreply.github.com>
2022-11-03 12:06:22 +0530

    Add files via upload

---
+++ Realtime Object Identification app/ADV-C135-Student/AI-Video-Surveillance/main.js
@@ -0,0 +1,53 @@
+video=""
+status=""
+objects=[]
+//we will write square brackets instea of "" for array
+function preload(){
+    video = createVideo("video.mp4")
+}
+function setup(){
+ canvas = createCanvas(480,380)
+ canvas.center()
+ video.hide()
+}
+function draw(){
+  image(video,0,0,480,380)
+
+  if(status != ""){
+    objectDetector.detect(video,gotResult)
+    for(var i=0;i<objects.length;i++){
+
+    document.getElementById(number_of_objects).innerHTML = "number of objects detected are :" + objects.length
+    document.getElementById(status).innerHTML = "status : object detected"
+
+    fill("red")
+     percent = floor(objects[i].confidence * 100)
+    
+    text(obejcts[i].label + " "  + percent + "%" , objects[i].x + 15 , objects[i].y + 15)
+    noFill()
+    stroke("red")
+    rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height)
+  }
+}
+
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded)
+    document.getElementById("status").innerHTML="status:detecting objects"
+}
+function modelLoaded(){
+  console.log("modelLoaded")
+  status=true;
+  video.loop();
+  video.speed(2.5);
+  video.volume(1);
+}
+function gotResult(error,results){
+  if(error){
+  console.error(error)
+  }
+  else{
+  console.log(results)
+  objects = results
+  }
+}
